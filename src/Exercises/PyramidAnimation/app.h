f87120f3592f2c62da438bb2120662a2d82a3d55
savicci <koziol.grzesiek@interia.pl>
2020-11-28 13:44:01 +0100

    PyramidAnimation: start

---
+++ src/Exercises/PyramidAnimation/app.h
@@ -0,0 +1,55 @@
+//
+// Created by pbialas on 05.08.2020.
+//
+
+
+#pragma once
+
+#include <vector>
+#include <glm/mat4x4.hpp>
+
+
+#include "Application/application.h"
+#include "Application/utils.h"
+
+#include "glad/glad.h"
+#include "camera.h"
+#include "camera_controller.h"
+
+class SimpleShapeApplication : public xe::Application {
+public:
+    SimpleShapeApplication(int width, int height, std::string title, int major = 4, int minor = 1) :
+            Application(width, height, title, major, minor) {}
+
+    void init() override;;
+
+    void frame() override;
+
+    void framebuffer_resize_callback(int w, int h) override;
+    void mouse_button_callback(int button, int action, int mods) override;
+    void cursor_position_callback(double x, double y) override;
+
+    void scroll_callback(double xoffset, double yoffset) override {
+        Application::scroll_callback(xoffset, yoffset);
+        camera()->zoom(yoffset / 30.0f);
+    }
+
+    void set_camera(Camera *camera) { camera_ = camera; }
+
+    void set_controler(CameraControler *controler) { controler_ = controler; }
+    Camera *camera() { return camera_; }
+
+    ~SimpleShapeApplication() {
+        delete camera_;
+    }
+
+private:
+    GLuint vao_;
+    GLuint u_pvm_buffer_;
+    Camera *camera_;
+    CameraControler *controler_;
+    unsigned int program_;
+
+    void preparePVM();
+    void setPVMUniformBufferData(const glm::mat4 &PVM) const;
+};
---
+++ src/Exercises/PyramidAnimation/shaders/base_vs.glsl
@@ -0,0 +1,15 @@
+#version 410
+
+layout(location=0) in  vec4 a_vertex_position;
+layout(location=1) in vec3 a_vertex_color;
+
+out vec3 vertex_color;
+
+uniform PVM {
+    mat4 proj_matrix;
+};
+
+void main() {
+    gl_Position = proj_matrix * a_vertex_position;
+    vertex_color = a_vertex_color;
+}
